2aada448eef67b809a5ffe758c807e3d2176a9c6
DRL\heych <D@rr3nH3lm>
2015-05-27 17:00:53 +0100

    Create a custom bootstrapper to set up the brighter command processor and related dependencies

--- src/Brighter.CircuitBreaker.Service/Processor.cs
+++ src/Brighter.CircuitBreaker.Service/Processor.cs
@@ -1,6 +1,21 @@
-﻿namespace Brighter.CircuitBreaker.Service
+﻿using paramore.brighter.commandprocessor;
+
+namespace Brighter.CircuitBreaker.Service
 {
     public class Processor
     {
+	    private readonly IAmACommandProcessor _commandProcessor;
+	    private readonly IAmACommandProcessorBuilder _commandProcessorBuilder;
+
+	    public Processor(IAmACommandProcessor commandProcessor, IAmACommandProcessorBuilder commandProcessorBuilder)
+	    {
+		    _commandProcessor = commandProcessor;
+		    _commandProcessorBuilder = commandProcessorBuilder;
+	    }
+
+	    public void Process()
+	    {
+		    //var commandProcessor = 
+	    }
     }
 }
---
+++ src/Brighter.CircuitBreaker.Web/Bootstrapper.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Brighter.CircuitBreaker.Service;
+using Nancy;
+using Nancy.Bootstrapper;
+using Nancy.TinyIoc;
+using paramore.brighter.commandprocessor;
+using paramore.brighter.commandprocessor.Logging;
+using Polly;
+
+namespace Brighter.CircuitBreaker.Web
+{
+	public class Bootstrapper : DefaultNancyBootstrapper
+	{
+		protected override void ApplicationStartup(TinyIoCContainer container, IPipelines pipelines)
+		{
+			var logger = LogProvider.For<Processor>();
+			container.Register(logger);
+
+			var circuitBreakerPolicy = Policy.Handle<Exception>().CircuitBreaker(1, TimeSpan.FromSeconds(10));
+
+			var subscriberRegistry = new SubscriberRegistry();
+			var handlerFactory = new TinyIoCHandlerFactory(container);
+			var requestContextFactory = new InMemoryRequestContextFactory();
+			var policyRegistry = new PolicyRegistry {{"circuitBreaker", circuitBreakerPolicy}};
+			var commandProcessor = new CommandProcessor(subscriberRegistry, handlerFactory, requestContextFactory, policyRegistry,
+				logger);
+		}
+	}
+
+	public class TinyIoCHandlerFactory : IAmAHandlerFactory
+	{
+		private readonly TinyIoCContainer _container;
+
+		public TinyIoCHandlerFactory(TinyIoCContainer container)
+		{
+			_container = container;
+		}
+
+		public IHandleRequests Create(Type handlerType)
+		{
+			return (IHandleRequests) _container.Resolve(handlerType);
+		}
+
+		public void Release(IHandleRequests handler)
+		{
+			var disposableHandler = handler as IDisposable;
+			if (disposableHandler != null)
+				disposableHandler.Dispose();
+
+			handler = null;
+		}
+	}
+}
